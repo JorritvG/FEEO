f4ed1da0afa6afa19b539c544480eb491b60dd0e
Jorrit van Geffen <jorritvgeffen@gmail.com>
2020-11-05 16:09:53 +0100

    TABEL INVULLEN DOET HET YESSSSSSSSSSSSSSSSSSSSSSSSS

--- Opdracht2-Website/scripts.js
+++ Opdracht2-Website/scripts.js
@@ -12,22 +12,23 @@ function myFunction() {
     bezoeker.email = email
     bezoeker.leeftijd = leeftijd
     bezoeker.woonplaats = woonplaats
+
+    let voornaam2 = document.getElementById("voornaam")
+    let achternaam2 = document.getElementById("achternaam")
+    let email2 = document.getElementById("email")
+    let leeftijd2 = document.getElementById("leeftijd")
+    let woonplaats2 = document.getElementById("woonplaats")
+
+    console.log(voornaam)
+    console.log(achternaam)
+    console.log(email)
+    console.log(leeftijd)
+    console.log(woonplaats)
+
+    voornaam2.innerText = bezoeker.voornaam
+    achternaam2.innerText = bezoeker.achternaam
+    email2.innerText = bezoeker.email
+    leeftijd2.innerText = bezoeker.leeftijd
+    woonplaats2.innerText = bezoeker.woonplaats
+
 }
-
-let voornaam = document.getElementById("voornaam")
-let achternaam = document.getElementById("achternaam")
-let email = document.getElementById("email")
-let leeftijd = document.getElementById("leeftijd")
-let woonplaats = document.getElementById("woonplaats")
-
-console.log(voornaam)
-console.log(achternaam)
-console.log(email)
-console.log(leeftijd)
-console.log(woonplaats)
-
-voornaam.innerText = bezoeker.voornaam
-achternaam.innerText = bezoeker.achternaam
-email.innerText = bezoeker.email
-leeftijd.innerText = bezoeker.leeftijd
-woonplaats.innerText = bezoeker.woonplaats
